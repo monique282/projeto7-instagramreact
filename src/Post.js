049117364fd4d73ebab9d2505a6435b193f84ef5
monique <moniquejp16@gmail.com>
2023-05-08 17:41:59 -0300

    so falta os dados dinamicos 1.3

--- src/Post.js
+++ src/Post.js
@@ -1,78 +1,86 @@
 import React, { useState } from "react";
 
-export default function Post() {
+export default function Post(props) {
+
+    
+
+    let [iconeSalvar, setIconeSalvar]= useState('bookmark-outline')
+    let [curtida, setCurtir] = useState('preto');
+    let [coracao, setcoracao] = useState('heart-outline');
+    let [NCurtidas, setNCurtidas]= useState(props.post.numeroCurtidas);
+    
 
 
-   
-    let [postSalvar, setPostAtualizado] = useState('');
-    let [curtida, setCurtir] = useState('branco');
 
     function salvarPost() {
         
-        if (postSalvar === '') {
-            setPostAtualizado('pretoSalvar');
+        if (iconeSalvar === 'bookmark-outline') {
+            setIconeSalvar('bookmark');
         } else {
-            setPostAtualizado('');
+            setIconeSalvar('bookmark-outline');
         }
     }
 
     function curt() {
-        if (curtida === 'branco') {
+        if (curtida === 'preto') {
+            setcoracao("heart");
             setCurtir('vermelho');
+            setNCurtidas(NCurtidas+1);
+            
         } else {
-            setCurtir('branco');
+            setCurtir('preto');
+            setcoracao('heart-outline');
+            setNCurtidas(NCurtidas-1);
         }
     }
 
     function curtirPost() {
         if (curtida === 'preto') {
             setCurtir('vermelho');
-
+            setcoracao("heart");
+            setNCurtidas(NCurtidas+1);
         }
     }
 
-    const post = [
-        { imgUsuario: "./assets/img/meowed.svg", altu: 'meowed', nomeu: 'meowed', imgPost: "./assets/img/gato-telefone.svg", altp: 'gato-telefone', imgCurtida: "./assets/img/respondeai.svg", altc: 'respondeAi' },
-        { imgUsuario: "./assets/img/barked.svg", altu: 'barked', nomeu: ' barked', imgPost: "./assets/img/dog.svg", altp: 'dog', imgCurtida: "./assets/img/adorable_animals.svg", altc: 'adorable_animals' },
-        { imgUsuario: "./assets/img/meowed.svg", altu: 'meowed', nomeu: 'meowed', imgPost: "./assets/img/gato-telefone.svg", altp: 'gato-telefone', imgCurtida: "./assets/img/respondeai.svg", altc: 'respondeAi' }
-    ];
+
+
+    
 
     return (
-         post.map(post =>
     (<div class="post" data-test="post">
         <div class="topo">
             <div class="usuario">
-                <img src={post.imgUsuario} alt={post.altu} />
-                {post.nomeu}
+                <img src={props.post.imgUsuario} alt={props.post.altu} />
+                {props.post.nomeu}
             </div>
             <div class="acoes">
                 <ion-icon name="ellipsis-horizontal"></ion-icon>
             </div>
         </div>
         <div class="conteudo">
-            <img src={post.imgPost} alt={post.altp} onClick={curtirPost} data-test="post-image"/>
+            <img src={props.post.imgPost} alt={props.post.altp} onClick={curtirPost} data-test="post-image"/>
         </div>
 
         <div class="fundo">
             <div class="acoes">
                 <div>
-                    <ion-icon onClick={curt} class={curtida} name="heart-outline" data-test="like-post"></ion-icon>
+                   <spam onClick={curt} class={curtida} data-test="like-post" > <ion-icon name={coracao} ></ion-icon></spam> 
                     <ion-icon name="chatbubble-outline"></ion-icon>
                     <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div>
-                    <ion-icon onClick={salvarPost} class={postSalvar} name="bookmark-outline" data-test="save-post" ></ion-icon>
+                    <ion-icon onClick={salvarPost} name={iconeSalvar} data-test="save-post" ></ion-icon>
                 </div>
             </div>
 
             <div class="curtidas">
-                <img src={post.imgCurtida} alt={post.altc} />
+                <img src={props.post.imgCurtida} alt={props.post.altc} />
                 <div class="texto">
-                    Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
+                    Curtido por <strong>adorable_animals</strong> e <strong>outras <span data-test="likes-number">{NCurtidas.toLocaleString('pt-br')}</span> pessoas</strong>
                 </div>
             </div>
         </div>
-    </div>)))
+    </div>))
 
 }
 
--- src/Posts.js
+++ src/Posts.js
@@ -1,12 +1,20 @@
 
 import Post from "./Post";
-export default function Posts() {
 
+export default function Posts() {
+    const post = [
+        { imgUsuario: "./assets/img/meowed.svg", altu: 'meowed', nomeu: 'meowed', imgPost: "./assets/img/gato-telefone.svg", altp: 'gato-telefone', imgCurtida: "./assets/img/respondeai.svg", altc: 'respondeAi', numeroCurtidas: 1982 },
+        { imgUsuario: "./assets/img/barked.svg", altu: 'barked', nomeu: ' barked', imgPost: "./assets/img/dog.svg", altp: 'dog', imgCurtida: "./assets/img/adorable_animals.svg", altc: 'adorable_animals',numeroCurtidas: 1863 },
+        { imgUsuario: "./assets/img/meowed.svg", altu: 'meowed', nomeu: 'meowed', imgPost: "./assets/img/gato-telefone.svg", altp: 'gato-telefone', imgCurtida: "./assets/img/respondeai.svg", altc: 'respondeAi', numeroCurtidas: 8951 }
+    ];
 
     return (
         <div class="posts">
-           <Post/>
+            {post.map((post)=> {
+return(<Post post = {post} />);
+            })}
+
         </div>
     );
 
-    }
+}
